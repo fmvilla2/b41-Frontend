0d45ec7a0ae6ad9bfb619e90c5240892337bc0a4
FernandoVilla <fmvilla@email.arizona.edu>
2020-06-12 20:51:45 -0500

    nothing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navigation from './components/Navigation';
 
 
 
 function App() {
   return (
     <React.Fragment>
+      <Navigation />
       <h1> Hello World </h1>
     </React.Fragment>
   );
